de0259ca182f37c3a2a59779a556c4c28f1a3f5c
曾佳俊 <952432141@qq.com>
2023-12-22 15:33:15 +0800

    chore: 添加 uno.css

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import router from "./router";
 
 import "animate.css";
 
+import "virtual:uno.css";
+
 const app = createApp(App);
 
 app.use(createPinia());
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,7 @@
 <script setup></script>
 
 <template>
-  <h1
-    class="animate__animated animate__zoomInDown"
-    style="font-size: 72px; color: #e69138"
-  >
+  <h1 class="animate__animated animate__zoomInDown text-72px color-#e69138">
     你好！
   </h1>
 </template>
