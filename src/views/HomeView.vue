96b286be6c43789609d9bcac95c5c7e0c9290afa
zengjiajun <952432141@qq.com>
2023-12-29 20:40:27 +0800

    feat: [homeview] 代码清理

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,77 +1,18 @@
 <template>
-  <div class="photo-mosaic">
-    <div ref="mosaic" class="mosaic-container">
-      <img
-        v-for="(photo, index) in photos"
-        :key="index"
-        :src="photo.src"
-        :style="{
-          width: photo.width + 'px',
-          height: photo.height + 'px',
-          objectFit: 'cover',
-        }"
-      />
-    </div>
+  <div class="home-view">
+    <div class="paper"></div>
   </div>
 </template>
 
-<script setup>
-import { ref, onMounted } from "vue";
-
-const photos = ref([]);
-const arrangedPhotos = ref([]);
-const number = ref("1995");
-
-import { getImageInfo } from "@/utils/importImages";
-
-onMounted(async () => {
-  // generateMosaic();
-  getImageInfo((imgArray) => {
-    photos.value = imgArray;
-  });
-});
-
-const generateMosaic = () => {
-  const mosaicContainer = document.querySelector(".mosaic-container");
-  const numberString = number.value.toString();
-
-  // Function to arrange photos based on number shape
-  const arrangePhotosByNumber = (photos, numberString) => {
-    // Logic to arrange photos based on number shape
-    // ...
-
-    // Return an array of arranged photos
-    return arrangedPhotosArray;
-  };
-
-  // Function to dynamically resize and place photos based on aspect ratio
-  const placePhotosDynamically = (photos) => {
-    // Logic to dynamically resize and place photos based on aspect ratio
-    // ...
-
-    // Modify arrangedPhotos array with dynamically placed photos
-    arrangedPhotos.value = dynamicallyPlacedPhotos;
-  };
-
-  // Call the functions to arrange and place photos
-  arrangedPhotos.value = arrangePhotosByNumber(photos, numberString);
-  placePhotosDynamically(arrangedPhotos.value);
-};
-</script>
+<script setup></script>
 
 <style scoped>
-/* Add necessary styles for the mosaic container and images */
-.photo-mosaic {
-  width: 400px;
-  height: 400px;
-  overflow: hidden;
-}
-
-.mosaic-container {
+.home-view {
   display: flex;
-  flex-wrap: wrap;
-  /* Add other necessary styles */
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(90deg, #ffe8c3 0%, #ead8b9 98.94%);
 }
-
-/* Add styles for individual images if needed */
 </style>
